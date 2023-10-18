098c451af83df68dee1728701c3f00a9d12aa93d
aikoijin <146685992+aikoijin@users.noreply.github.com>
2023-10-17 23:49:08 -0700

    CSS IMG and HTML

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -19,3 +19,27 @@ function main() {
 
 // let's get this party started
 main();
+
+// Rock Paper Scissors
+// An implementation of the game
+// Michelle Wang <mwang180@ucsc.edu> & Ashley Pham <aspham@ucsc.edu>
+// October 2023
+
+// countdown = ["rock, paper, scissors, shoot!"]
+
+
+// find_opponent
+// print("how many rounds do you want to play?")
+// rounds = input()
+
+// start.countdown 
+// player.input = (rock, paper, OR scissors)
+// opponent.input = (rock, paper, OR scissors)
+// if (player.input == rock and opponent.input == scissors) or
+//     (player.input == paper and opponent.input == rock) or
+//     (player.input == scissors and opponent.input == paper):
+//     print("you win!")
+// else if (player.input == opponent.input)
+//     print("tie game")
+// else
+//     print("you lose")
