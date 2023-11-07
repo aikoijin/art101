7c090040102288124bc3e08844e925834d338fbd
aikoijin <146685992+aikoijin@users.noreply.github.com>
2023-11-06 16:42:26 -0800

    CSS and HTML

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -12,16 +12,30 @@ function mySquare(x){
 console.log("The square of 10 is: ", mySquare(10));
 console.log("The square of 124 is: ", mySquare(124));
 
-array = [129,41,432,8593,291]
+var array = [129,41,432,8593,291];
 console.log("My array: ", array);
 
 var result = array.map(mySquare);
 console.log("Test of squares of array: ", result);
 
-var result = array.map(function(x){
-    var results = x%2 == 0;
-    return results;
+var isEven = array.map(function(x){
+    var even = x%2 == 0;
+    return even;
 })
 
+
+
 //output for eveness (true or false)
-console.log("Eveness of array: ", result);
+console.log("Eveness of array: ", isEven);
+
+//bonus task
+function main(){
+    console.log("Main function started.");
+    //using get element ID to print
+    document.getElementById("array").innerHTML = "My array: " + array;
+    document.getElementById("even").innerHTML = "Is my array even?: " + isEven;
+    document.getElementById("squared").innerHTML = "The squared of my array: " + result; 
+}
+
+//let's get this party started
+window.onload = main;
