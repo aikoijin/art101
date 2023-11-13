eded41e84fe77517385918cceb778d9081c667e3
aikoijin <146685992+aikoijin@users.noreply.github.com>
2023-11-12 16:10:35 -0800

    lab9

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -54,7 +54,7 @@ $("#challenge, #problems, #results").addClass("special-sections");
 
 $(".special-sections").append("<button class='toggle-button'>Click Here!</button>");
 
-$(".special-sections button").click(function(){
+$(".special-sections button").one('click', function(){
     var sectionId = $(this).parent().attr('id');
     var paragraphText = "";
 
@@ -62,11 +62,11 @@ $(".special-sections button").click(function(){
         paragraphText = "The challenge of the lab was to experiment with jQuery and make buttons that have an action when clicked.";
     } else if (sectionId === "problems") {
         paragraphText = `Some problems that came up was getting the buttons to do something. I managed to get challenge to click and then show a message.
-        For the other buttons I decided to change the color of the section similar to the professor's website "highlight". This proved difficult at
-        first but after looking over the JS and understanding how it works, it was easier. I also decided to try doing a collapsible button for one of my
-        sections and used W3schools to incorporate it into my code.`;
+        However, this proved difficult because it would change the whole section to the text "You clicked it!" instead of just the button.
+        After looking over the JS with Ashley and understanding how it works, it was easier. I also did some research to make the button only clickable once so
+        that we did not see the content multiple times everytime we clicked the button. I decided to highlight the section when it was revealed as well.`;
     } else if (sectionId === "results") {
-        paragraphText = "You can see the results if you click the button that says 'Click Here' in the challenge section. I also decided to change the section backgrounds to test out other button functions.";
+        paragraphText = "You can see the results if you click the button that says 'Click Here' in the following sections.";
     }
 
     // appends a new paragraph to the parent section when clicked
