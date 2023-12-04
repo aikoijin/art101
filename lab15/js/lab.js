9f1f8159b1abd2635816e76899ba710e9f3feaaf
aikoijin <146685992+aikoijin@users.noreply.github.com>
2023-12-03 16:41:05 -0800

    lab15

---
+++ lab15/js/lab.js
@@ -0,0 +1,26 @@
+/*
+   lab.js - Using AJAX to grab info from external sources and implement them into our page
+
+   Requirements: jQuery must be loaded for this script to work.
+
+   Author: Michelle Wang
+   Date: December 3rd, 2023
+*/
+
+//Choose Your Pokemon!
+document.getElementById('activate').addEventListener('click', function() {
+  var randomPokemon = Math.floor(Math.random() * 898) + 1;
+  fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemon}`)
+  .then(response => response.json())
+  .then(data => {
+      document.getElementById('output2').innerHTML = `
+      <h3>${data.name}</h3>
+      <p>${data.types[0].type.name}</p>
+      <p><img src="${data.sprites.front_default}" alt="${data.name}"></p>`;
+  })
+  .catch(error => console.log(error));
+}) 
+
+document.getElementById('pokeball').addEventListener('click', function() {
+  document.getElementById('activate').click();
+});
